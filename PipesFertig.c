60326278d395491c9c4e3cecbd277dce8cffec5e
kello63 <131592601+kello63@users.noreply.github.com>
2023-06-23 20:06:42 +0200

    Update PipesFertig.c

--- PipesFertig.c
+++ PipesFertig.c
@@ -13,14 +13,13 @@ void
 intHandler (int signal)
 {
   keepRunning = 0;
-
 }
 
 int
 main ()
 {
 
-  //Bei 'CTRl-C'  wird Signal-Handler aktiviert
+  //Bei 'CTRl-C' wird Signal-Handler aktiviert
   signal (SIGINT, intHandler);
 
   //Endlosprozesse werden gestartet
@@ -29,8 +28,23 @@ main ()
 
       //erste Pipe fC<r Messwerte
       int fd[2];
-
       if (pipe (fd) == -1)
+	{
+	  printf ("\nerror");
+	  exit (1);
+	}
+
+      //zweite Pipe fC<r Summe
+      int fd1[2];
+      if (pipe (fd1) == -1)
+	{
+	  printf ("\nerror");
+	  exit (1);
+	}
+
+      //dritte Pipe fC<r Mittelwert
+      int fd2[2];
+      if (pipe (fd2) == -1)
 	{
 	  printf ("\nerror");
 	  exit (1);
@@ -43,11 +57,13 @@ main ()
 	  //Conv-Prozess
 	  int werte[5];
 
+	  //Zufalls-Messwerte werden erzeugt
 	  for (int i = 0; i < 5; i++)
 	    {
 	      werte[i] = rand () % 101;
 	    }
 
+	  //Messwerte in Pipe schreiben
 	  close (fd[0]);
 	  write (fd[1], werte, sizeof (int) * 5);
 	  close (fd[1]);
@@ -55,40 +71,14 @@ main ()
       else
 	{
 
-	  //Stat-Prozess
+	  //Log-Prozess
+
+	  //Messwerte von Conv aus Pipe lesen
 	  close (fd[1]);
-	  int werte[5];
-	  read (fd[0], werte, sizeof (int) * 5);
+	  int werte1[5];
+	  read (fd[0], werte1, sizeof (int) * 5);
 	  close (fd[0]);
 
-	  int summe = 0;
-
-	  for (int i = 0; i < 5; i++)
-	    {
-	      summe += werte[i];
-	    }
-
-	  int mittelwert = summe / 5;
-
-	  //zweite Pipe fC<r Summe
-	  int fd1[2];
-
-	  if (pipe (fd1) == -1)
-	    {
-	      printf ("\nerror");
-	      exit (1);
-	    }
-
-	  //dritte Pipe fC<r Mittelwert
-	  int fd2[2];
-
-	  if (pipe (fd2) == -1)
-	    {
-	      printf ("\nerror");
-	      exit (1);
-	    }
-
-	  //Log-Prozess
 	  FILE *fp;
 
 	  //Lokale Datei C6ffnen
@@ -101,9 +91,10 @@ main ()
 	  //Messwerte aus Conv reinschreiben
 	  for (int i = 0; i < 5; i++)
 	    {
-	      fprintf (fp, "Messwert:  %.2d\n", werte[i]);
+	      fprintf (fp, "Messwert:  %.2d\n", werte1[i]);
 
 	    }
+	  //Lokale Datei schliessen
 	  fclose (fp);
 
 	  //Warten auf Conv-Prozess
@@ -113,13 +104,33 @@ main ()
 
 	  if (id1 == 0)
 	    {
+	      //Stat-Prozess
 
-	      //Summe reinschreiben
+	      //Messwerte von Conv aus Pipe lesen
+	      close (fd[1]);
+	      int werte2[5];
+	      read (fd[0], werte2, sizeof (int) * 5);
+	      close (fd[0]);
+
+	      //Summe berechnen
+	      int summe = 0;
+
+	      for (int i = 0; i < 5; i++)
+		{
+		  summe += werte2[i];
+		}
+
+	      //Mittrlwert berechnen
+	      int mittelwert = summe / 5;
+
+
+
+	      //Summe in Pipe schreiben
 	      close (fd1[0]);
 	      write (fd1[1], &summe, sizeof (int));
 	      close (fd1[1]);
 
-	      //Mittelwert reinschreiben
+	      //Mittelwert in Pipe schreiben
 	      close (fd2[0]);
 	      write (fd2[1], &mittelwert, sizeof (int));
 	      close (fd2[1]);
@@ -129,20 +140,20 @@ main ()
 	    {
 	      //Report-Prozess
 	      //Summe auslesen
-	      int summe;
+	      int sum;
 	      close (fd1[1]);
-	      read (fd1[0], &summe, sizeof (int));
+	      read (fd1[0], &sum, sizeof (int));
 	      close (fd1[0]);
 
 	      //Mittelwert auslesen
-	      int mittelwert;
+	      int mw;
 	      close (fd2[1]);
-	      read (fd2[0], &mittelwert, sizeof (int));
+	      read (fd2[0], &mw, sizeof (int));
 	      close (fd2[0]);
 
 	      //Summe und Mittelwert ausgeben
-	      printf ("\nSumme: %d", summe);
-	      printf ("\nMittelwert: %d", mittelwert);
+	      printf ("\nSumme: %d", sum);
+	      printf ("\nMittelwert: %d", mw);
 	      printf ("\n");
 
 	      //Warten auf Stat-Prozess 
@@ -150,7 +161,6 @@ main ()
 	    }
 
 	}
-
 
     }
   return 0;
